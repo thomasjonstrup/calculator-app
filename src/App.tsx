d98cd728c5841f3993e154fbeda66ca6f4364a71
Thomas Jonstrup <thomas_jonstrup@hotmail.com>
2021-05-15 16:09:37 +0200

    core functionality
    Added core functionality and start styling for calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,218 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./scss/main.scss";
+
+interface calcButton {
+	value: string;
+	displayValue: string;
+	type: string;
+}
+
+export const formatNumber = (inputNumber: string) => {
+	let formetedNumber = Number(inputNumber)
+		.toFixed(2)
+		.replace(/\d(?=(\d{3})+\.)/g, "$&,");
+	let splitArray = formetedNumber.split(".");
+	if (splitArray.length > 1) {
+		formetedNumber = splitArray[0];
+	}
+	return formetedNumber;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [calcValue, setCalcValue] = useState("");
+	const [calculatedValue, setCalculatedValue] = useState("");
+
+	const calcButtons: calcButton[] = [
+		{
+			value: "7",
+			displayValue: "7",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "8",
+			displayValue: "8",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "9",
+			displayValue: "9",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "del",
+			displayValue: "del",
+			type: "btn btn--tetriary btn--del",
+		},
+		{
+			value: "4",
+			displayValue: "4",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "5",
+			displayValue: "5",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "6",
+			displayValue: "6",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "+",
+			displayValue: "+",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "1",
+			displayValue: "1",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "2",
+			displayValue: "2",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "3",
+			displayValue: "3",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "-",
+			displayValue: "-",
+			type: "btn btn--secondary",
+		},
+		{
+			value: ".",
+			displayValue: ".",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "0",
+			displayValue: "0",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "/",
+			displayValue: "/",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "*",
+			displayValue: "x",
+			type: "btn btn--secondary",
+		},
+		{
+			value: "reset",
+			displayValue: "reset",
+			type: "btn btn--tetriary btn--reset",
+		},
+		{
+			value: "=",
+			displayValue: "=",
+			type: "btn btn--primary btn--equal",
+		},
+	];
+
+	const onChange = (event: React.FormEvent<HTMLInputElement>): void => {
+		if (isNaN(event.currentTarget.value as any)) {
+			return;
+		}
+		setCalcValue(event.currentTarget.value);
+	};
+
+	const onClick = (event: React.FormEvent<HTMLButtonElement>): void => {
+		if (event.currentTarget.value === "del") {
+			const newValue = calcValue.slice(0, -1);
+
+			return setCalcValue(newValue);
+		}
+		if (event.currentTarget.value === "reset") {
+			setCalculatedValue("");
+			setCalcValue("");
+
+			return;
+		}
+		if (event.currentTarget.value === "=") {
+			try {
+				// eslint-disable-next-line
+				const value = eval(calcValue);
+
+				return setCalculatedValue(value);
+			} catch (error) {
+				return setCalculatedValue("");
+			}
+		}
+
+		setCalcValue(calcValue + event.currentTarget.value);
+
+		if (calculatedValue) {
+			setCalculatedValue("");
+		}
+	};
+
+	return (
+		<div className="App">
+			<main>
+				<div className="container">
+					<div className="calculator">
+						<div className="calculator__top">
+							<div className="calculator__inner">
+								<div className="calculator__left">calc</div>
+								<div className="calculator__right">Theme</div>
+							</div>
+							<input
+								type="text"
+								className="calculator__input"
+								value={
+									calculatedValue
+										? calculatedValue
+										: calcValue
+								}
+								onChange={onChange}
+							/>
+						</div>
+						<div className="calculator__content">
+							{calcButtons.map(
+								(button: calcButton, index: number) => {
+									return (
+										<button
+											className={button.type}
+											onClick={onClick}
+											value={button.value}
+											key={index}
+										>
+											{button.displayValue}
+										</button>
+									);
+								}
+							)}
+						</div>
+					</div>
+				</div>
+			</main>
+			<footer className="footer">
+				<div className="container">
+					<div className="attribution">
+						Challenge by
+						<a
+							href="https://www.frontendmentor.io?ref=challenge"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Frontend Mentor
+						</a>
+						. Coded by
+						<a href="https://github.com/thomasjonstrup">
+							Thomas Jonstrup
+						</a>
+						.
+					</div>
+				</div>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
